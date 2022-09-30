7b45f59e7f8ed40321ad20aab0352cf2e096e06f
externalcharm <freesxsad23@mail.ru>
2022-09-30 22:49:26 +0300

    homework with task4

--- lesson_1/task4/Program.cs
+++ lesson_1/task4/Program.cs
@@ -2,3 +2,15 @@
 
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
+
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+while (count <= number)
+{
+    if (count % 2 == 0)
+    {
+        System.Console.WriteLine(count);
+    }
+    count++;
+}
