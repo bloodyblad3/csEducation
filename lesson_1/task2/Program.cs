85b8f2747abdc9a59c96275154a2617be574eba5
externalcharm <freesxsad23@mail.ru>
2022-09-30 22:39:13 +0300

    homework with task2

--- lesson_1/task2/Program.cs
+++ lesson_1/task2/Program.cs
@@ -4,3 +4,31 @@
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
+
+System.Console.WriteLine("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите третье число: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+int min = 0;
+int max = 0;
+
+if (numberA > numberB && numberA > numberC)
+{
+    max = numberA;
+    min = (numberB < numberC) ? numberB : numberC;
+    System.Console.WriteLine($"Среди трех чисел, максимальное = {max}, а минимальное = {min}");
+}
+else if (numberB > numberA && numberB > numberC)
+{
+    max = numberB;
+    min = (numberA < numberC) ? numberA : numberC;
+    System.Console.WriteLine($"Среди трех чисел, максимальное = {max}, а минимальное = {min}");
+}
+else
+{
+    max = numberC;
+    min = (numberB < numberA) ? numberB : numberA;
+    System.Console.WriteLine($"Среди трех чисел, максимальное = {max}, а минимальное = {min}");
+}
