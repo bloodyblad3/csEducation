9823d284c4ab495cb58bdafb8adac6574043dd32
externalcharm <freesxsad23@mail.ru>
2022-10-04 23:12:16 +0300

    homework with task4

--- lesson_2/task4/Program.cs
+++ lesson_2/task4/Program.cs
@@ -3,3 +3,24 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+System.Console.WriteLine("Введите цифру от 1 до 7: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void CheckTheDayOfTheWeek(int number)
+{
+    if (number == 6 || number == 7)
+    {
+        System.Console.WriteLine("да");
+    }
+    if (number < 1 || number > 7)
+    {
+        System.Console.WriteLine("Такого дня недели не существует!");
+    }
+    if (number < 6) // знаю что можно через else, но почему-то выводило да-нет при вводе числа 6 или 7, поэтому заменил на конкретное условие
+    {
+        System.Console.WriteLine("нет");
+    }
+}
+
+CheckTheDayOfTheWeek(number);
