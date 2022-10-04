f11d3e606033c94a9b0e3d23789430ad54a49837
externalcharm <freesxsad23@mail.ru>
2022-10-04 23:03:32 +0300

    homework with task 3

--- lesson_2/task3/Program.cs
+++ lesson_2/task3/Program.cs
@@ -3,3 +3,14 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 326792 -> 6
+
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 100)
+{
+    System.Console.WriteLine(number % 10);
+}
+else
+{
+    System.Console.WriteLine("Третьей цифры нет");
+}
