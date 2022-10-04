ccb7083ed42250d401195551c8e36996a7816ed1
externalcharm <freesxsad23@mail.ru>
2022-10-04 22:28:38 +0300

    homework with task 1

--- lesson_2/task1/Program.cs
+++ lesson_2/task1/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. Не использовать строки для расчета.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+System.Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = (number / 10) % 10;
+System.Console.WriteLine($"Вторая цифра этого числа > {result}");
