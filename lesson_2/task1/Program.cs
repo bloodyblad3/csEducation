d1ce62312f40ea0fc574abf499a02b8d52e34800
externalcharm <freesxsad23@mail.ru>
2022-10-06 00:59:08 +0300

    condition added

--- lesson_2/task1/Program.cs
+++ lesson_2/task1/Program.cs
@@ -6,5 +6,12 @@
 
 System.Console.WriteLine("Введите трехзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-int result = (number / 10) % 10;
-System.Console.WriteLine($"Вторая цифра этого числа > {result}");
+if (number >= 100 && number <= 999)
+{
+    int result = (number / 10) % 10;
+    System.Console.WriteLine($"Вторая цифра этого числа > {result}");
+}
+else
+{
+    System.Console.WriteLine("Введено не трехзначное число");
+}
