d4b7588e6866f02adc13cd44a60cfa8f631cdf84
externalcharm <freesxsad23@mail.ru>
2022-10-07 23:06:28 +0300

    homework with task1

--- lesson_3/task1/Program.cs
+++ lesson_3/task1/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int number = Prompt("Введите пятизначное число: ");
+int temp = number;
+int reversed = 0;
+
+bool IsPolyndrom(int num)
+{
+    if (num >= 10000)
+    {
+        while (num > 0)
+        {
+            int dig = num % 10;
+            reversed = reversed * 10 + dig;
+            num = num / 10;
+            if (temp == reversed)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+    return false;
+}
+
+if (IsPolyndrom(number))
+{
+    System.Console.WriteLine("да");
+}
+else
+{
+    System.Console.WriteLine("Введено не пятизначное число, либо оно не является полиндромом");
+}
