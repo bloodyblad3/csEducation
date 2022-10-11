7dd436af4da6b7b9c2fa6e261f99633932904507
externalcharm <freesxsad23@mail.ru>
2022-10-11 23:31:11 +0300

    homework with task3

--- lesson_4/task3/Program.cs
+++ lesson_4/task3/Program.cs
@@ -1,3 +1,24 @@
 ﻿// Задача 3: Напишите программу, которая задаёт массив из 8 случайных элементов и выводит их на экран.
 
 // 1, 2, 5, 7, 19, 6, 7, 8
+
+int[] array = new int[10];
+
+int[] FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 20);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}\t");
+    }
+}
+
+PrintArray(FillArray(array));
