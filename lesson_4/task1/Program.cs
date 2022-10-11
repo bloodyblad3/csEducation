caa3c5948bb3c3f61b1f67cbb507ad2a00e0136d
externalcharm <freesxsad23@mail.ru>
2022-10-11 22:52:14 +0300

    homework with task1

--- lesson_4/task1/Program.cs
+++ lesson_4/task1/Program.cs
@@ -3,4 +3,25 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
 
+int numberA = Prompt("Введите число A: ");
+int numberB = Prompt("Введите число B: ");
+double pow = 1;
+
+double Power(int numA, int numB)
+{
+    for (int i = 0; i < numB; i++)
+    {
+        pow *= numA;
+    }
+    return pow;    
+}
+
+double result = Power(numberA, numberB);
+System.Console.WriteLine($"{result:f2}");
