b3ebae6d8650d3f3ea50a57afb87831b2b953287
externalcharm <freesxsad23@mail.ru>
2022-10-14 22:16:12 +0300

    homework with task2

--- lesson_5/task2/Program.cs
+++ lesson_5/task2/Program.cs
@@ -3,3 +3,40 @@
 // [3, 7, 23, 12] -> 19
 
 // [-4, -6, 89, 6] -> 0
+
+int[] CreateArray(int len, int minLimit, int maxLimit)
+{
+    int[] array = new int[len];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(minLimit, maxLimit + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}\t");
+    }
+    System.Console.WriteLine();
+}
+
+int SumOddNum(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 != 0)
+        {
+            sum += array[i];
+        }
+    }
+    return sum;
+}
+
+int[] array = CreateArray(10, 0, 20);
+PrintArray(array);
+System.Console.WriteLine($"Сумма нечетных чисел в массиве: {SumOddNum(array)}");
