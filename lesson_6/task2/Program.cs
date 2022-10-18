eb9f3d6e49189c8ed9eb25470df78726ccbef8d5
externalcharm <freesxsad23@mail.ru>
2022-10-18 22:30:20 +0300

    homework with task2

--- lesson_6/task2/Program.cs
+++ lesson_6/task2/Program.cs
@@ -1,3 +1,28 @@
 ﻿// Задача 2: Напишите программу, которая найдёт точку пересечения двух прямых, 
 // заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+const int x = 1;
+double Prompt(string msg)
+{
+    System.Console.WriteLine(msg);
+    double number = Convert.ToDouble(Console.ReadLine());
+    return number;
+}
+
+double PointX(double b1, double k1, double b2, double k2)
+{
+    return (b2 - b1) / (k1 - k2);
+}
+
+double PointY(double b1, double k1, double b2, double k2)
+{
+    return k1*(b2-b1)/(k1-k2)+b1;
+}
+
+double b1 = Prompt("Введите точку b1");
+double k1 = Prompt("Введите точку k1");
+double b2 = Prompt("Введите точку b2");
+double k2 = Prompt("Введите точку k2");
+
+System.Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> {PointX(b1, k1, b2, k2)}; {PointY(b1, k1, b2, k2)}");
