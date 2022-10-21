3579ffda8dca20cf433e188c3e9550c0d315e284
externalcharm <freesxsad23@mail.ru>
2022-10-21 21:58:35 +0300

    homework with task1

--- lesson_7/task1/Program.cs
+++ lesson_7/task1/Program.cs
@@ -3,3 +3,32 @@
 // 0,5 7 -2 -0,2
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
+
+double[,] CreateArray(int lines = 5, int columns = 5)
+{
+    double[,] array = new double[lines, columns];
+    Random random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(0,10) + random.NextDouble();
+        }
+    }
+    return array;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]:f2} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+double[,] array = CreateArray();
+PrintArray(array);
