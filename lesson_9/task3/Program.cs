29ac8d75ffc38a5c4d5f959831541e6afff4536e
externalcharm <freesxsad23@mail.ru>
2022-11-11 21:38:41 +0300

    homework with task3

--- lesson_9/task3/Program.cs
+++ lesson_9/task3/Program.cs
@@ -2,3 +2,28 @@
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
 
+int InputNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int AccermanFunc(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return AccermanFunc(m - 1, 1);
+    }
+    else
+    {
+        return AccermanFunc(m - 1, AccermanFunc(m, n - 1));
+    }
+}
+
+int m = InputNumber("Введите число M: ");
+int n = InputNumber("Введите число N: ");
+System.Console.WriteLine(AccermanFunc(m, n));
