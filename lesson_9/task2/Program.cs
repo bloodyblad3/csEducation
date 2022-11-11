45d90433f46b8457c6ad6a313d41a0a0cf725f17
externalcharm <freesxsad23@mail.ru>
2022-11-11 21:33:39 +0300

    homework with task2

--- lesson_9/task2/Program.cs
+++ lesson_9/task2/Program.cs
@@ -2,3 +2,25 @@
 
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
+
+int InputNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumNum(int m, int n)
+{
+    int start = m;
+    int end = n;
+    if (m > n)
+    {
+        start = n;
+        end = m;
+    }
+    return (end + start) * (end - start + 1) / 2;
+}
+
+int m = InputNumber("Введите число M: ");
+int n = InputNumber("Введите число N: ");
+System.Console.WriteLine($"Сумма в промежутке > {SumNum(m, n)}");
